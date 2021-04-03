0331bc5e34fa2c56dec85192e58027cfc2b37cdf
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2021-04-03 23:14:35 +0200

    Updated @pallad/scripts

--- src/EntityRef.ts
+++ src/EntityRef.ts
@@ -3,28 +3,28 @@ import * as isPred from 'predicates';
 const INSTANCE_SYMBOL = Symbol('entity_ref_instance');
 
 export interface EntityRef<TType> {
-    readonly type: TType;
+	readonly type: TType;
 }
 
 export namespace EntityRef {
-    export type Enchanted<T> = T & { [INSTANCE_SYMBOL]: true };
+	export type Enchanted<T> = T & { [INSTANCE_SYMBOL]: true };
 
-    /**
-     * Checks if provided value is entity ref of given type
-     */
-    export function is<T extends EntityRef<TType>, TType>(type: TType, value: any): value is Enchanted<T> {
-        //tslint:disable-next-line: strict-comparisons
-        return isPred.object(value) && value[INSTANCE_SYMBOL] === true && value.type === type;
-    }
+	/**
+	 * Checks if provided value is entity ref of given type
+	 */
+	export function is<T extends EntityRef<TType>, TType>(type: TType, value: any): value is Enchanted<T> {
+		//tslint:disable-next-line: strict-comparisons
+		return isPred.object(value) && value[INSTANCE_SYMBOL] === true && value.type === type;
+	}
 
-    /**
-     * Creates entity ref
-     */
-    export function create<T extends object, TType>(type: TType, data: T): Enchanted<EntityRef<TType> & T> {
-        return Object.freeze({
-            ...data,
-            type,
-            [INSTANCE_SYMBOL]: true
-        });
-    }
+	/**
+	 * Creates entity ref
+	 */
+	export function create<T extends object, TType>(type: TType, data: T): Enchanted<EntityRef<TType> & T> {
+		return Object.freeze({
+			...data,
+			type,
+			[INSTANCE_SYMBOL]: true
+		});
+	}
 }
